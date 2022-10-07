c890daae8f6cb8d1d97d4de7840e32608747d4a7
jvanbrie <jon.vanbriesen@datadoghq.com>
2022-10-07 13:58:40 -0400

    adding deck tracker

--- ai.py
+++ ai.py
@@ -2,41 +2,11 @@ from argparse import ArgumentParser
 import json
 from websocket import create_connection
 
-
-def action_phase(state):
-    playable = [card for card in state.hand if card in ["Market", "Village"]]
-    for card in playable:
-        play_card(card, state)
-
-
-def buy_phase(state):
-    playable = [card for card in state.hand if card in [
-        "Copper", "Silver", "Gold"]]
-    for card in playable:
-        play_card(card, state)
-    for card in find_cards_to_buy(state):
-        if state.supply[card] > 0:
-            buy_card(card, state)
-
-
-def find_cards_to_buy(state):
-    card_to_buy = "Province"
-    if state.treasure < 8:
-        card_to_buy = "Gold"
-    if state.treasure < 6:
-        card_to_buy = "Market"
-    if state.treasure < 5:
-        card_to_buy = "Silver"
-    if state.treasure < 3:
-        card_to_buy = "Copper"
-    return [card_to_buy]
-
-
 class State(object):
     def __init__(self) -> None:
         self.hand = []
         self.discard = 0
-        self.deck = []
+        self.deck = ["Copper"] * 7 + ["Estate"] * 3
         self.supply = {}
         self.buys = 1
         self.actions = 1
@@ -45,9 +15,69 @@ class State(object):
         self.conn = None
 
     def __repr__(self) -> str:
-        return "${} | {} actions | {} buys | {} discard | {} deck | hand: {}".format(
-            self.treasure, self.actions, self.buys, self.discard, self.deck, self.hand)
+        return "${} | {} actions | {} buys | {} discard | {} deck | hand: {} | avg_draw_wo_action: {}".format(
+            self.treasure, 
+            self.actions, 
+            self.buys, 
+            self.discard, 
+            self.deck, 
+            self.hand, 
+            self.average_value_draw_without_action(),
+            )
+    def average_value_draw_without_action(self) -> int:
+        total_value = 0
+        for card in self.deck:
+            if card == "Gold":
+                total_value += 3
+            if card == "Silver":
+                total_value += 3
+            if card == "Copper":
+                total_value += 3
+        return total_value / len(self.deck)
+            
 
+class Strategy(object):
+    def find_cards_to_buy(self, state: State) -> list[str]:
+        raise Exception("Not Implemented")
+
+class StrategyOne(Strategy):
+    def find_cards_to_buy(self, state: State) -> list[str]:
+        card_to_buy = "Province"
+        if state.treasure < 8:
+            card_to_buy = "Gold"
+        if state.treasure < 6:
+            card_to_buy = "Silver"
+        if state.treasure < 3:
+            card_to_buy = "Copper"
+        return [card_to_buy]
+
+class StrategyTwo(Strategy):
+    def find_cards_to_buy(self, state: State) -> list[str]:
+        card_to_buy = "Province"
+        if state.treasure < 8:
+            card_to_buy = "Gold"
+        if state.treasure < 6:
+            card_to_buy = "Market"
+        if state.treasure < 5:
+            card_to_buy = "Silver"
+        if state.treasure < 3:
+            return []
+        return [card_to_buy]
+
+def action_phase(state):
+    playable = [card for card in state.hand if card in ["Market", "Village"]]
+    for card in playable:
+        play_card(card, state)
+
+
+def buy_phase(state, strategy):
+    playable = [card for card in state.hand if card in [
+        "Copper", "Silver", "Gold"]]
+    for card in playable:
+        play_card(card, state)
+    for card in strategy.find_cards_to_buy(state):
+        if state.supply[card] > 0:
+            buy_card(card, state)
 
 def play_card(card, state):
     state.payload["method"] = "Play"
@@ -58,6 +88,7 @@ def play_card(card, state):
 
 
 def buy_card(card, state):
+    state.deck += [card]
     state.payload["method"] = "Buy"
     state.payload["params"] = {"card": card}
     print("Buying", card)
@@ -74,11 +105,9 @@ def action_response(state):
         parse_response(state, result)
         print(state)
 
-
 def parse_response(state, response):
     state.hand = response["hand"]
     state.discard = response["discard"]
-    state.deck = response["deck"]
     state.supply = response["supply"]
     if "buys" in response:
         state.buys = response["buys"]
@@ -95,8 +124,12 @@ def end_turn(state):
     state.conn.send(json.dumps(state.payload))
 
 
-def run_server(conn):
+def run_server(conn, player_number):
     state = State()
+    if player_number == 1:
+        strategy = StrategyOne()
+    else:
+        strategy = StrategyTwo()
     while True:
         payload = {
             "jsonrpc": "2.0",
@@ -120,7 +153,7 @@ def run_server(conn):
             state.payload = payload
             state.conn = conn
             action_phase(state)
-            buy_phase(state)
+            buy_phase(state, strategy)
             end_turn(state)
         elif method == "GameOver":
             print(response["params"])
@@ -146,7 +179,7 @@ def main(args):
 
     endpoint = parse_http_endpoint(args_dict["http_endpoint"])
     connection = make_connection(endpoint, args_dict["player"])
-    run_server(connection)
+    run_server(connection, int(args_dict["player"]))
 
 
 if __name__ == '__main__':
