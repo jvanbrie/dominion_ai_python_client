26f173b4e9dbe0771ca8045e9711f150530b51f8
jvanbrie <jon.vanbriesen@datadoghq.com>
2022-10-07 16:35:49 -0400

    adding support for militia

--- ai.py
+++ ai.py
@@ -57,16 +57,26 @@ class StrategyTwo(Strategy):
         if state.treasure < 8:
             card_to_buy = "Gold"
         if state.treasure < 6:
-            card_to_buy = "Market"
-        if state.treasure < 5:
+            if len(list(filter(lambda x: x == "Militia", state.deck))) < 2:
+                card_to_buy = "Militia"
+            else:
+                card_to_buy = "Silver"
+        if state.treasure < 4:
             card_to_buy = "Silver"
         if state.treasure < 3:
             return []
         return [card_to_buy]
 
 def action_phase(state):
-    playable = [card for card in state.hand if card in ["Market", "Village"]]
+    playable_prioritize = [card for card in state.hand if card in ["Market", "Village"]]
+    for card in playable_prioritize:
+        if state.actions < 1:
+            break
+        play_card(card, state)
+    playable = [card for card in state.hand if card in ["Militia"]]
     for card in playable:
+        if state.actions < 1:
+            break
         play_card(card, state)
 
 
@@ -122,6 +132,35 @@ def end_turn(state):
     if "params" in state.payload:
         state.payload.pop("params")
     state.conn.send(json.dumps(state.payload))
+    response = json.loads(state.conn.recv())
+    parse_response(state, response["result"])
+
+def handle_attack(state, request):
+    default_card_value = 1
+    card_to_value_in_hand = {
+        "Estate" : 0,
+        "Duchy": 0,
+        "Province": 0,
+        "Copper": 1,
+        "Silver": 2,
+        "Gold": 3,
+        "Market": 2,
+        "Militia": 2,
+    }
+    if request["card"] == "Militia":
+        if len(state.hand) > 3:
+            curr_hand = state.hand
+            curr_hand.sort(key = lambda x: card_to_value_in_hand.get(x, default_card_value))
+            cards_to_discard = curr_hand[:2]
+            state.payload["result"]["data"] = cards_to_discard
+            state.hand = curr_hand[2:]
+            state.conn.send(json.dumps(state.payload))
+        else:
+            state.payload["result"]["data"] = []
+            state.conn.send(json.dumps(state.payload))
+    else:
+        print("Unhandled Attack")
+        print(request)
 
 
 def run_server(conn, player_number):
@@ -155,9 +194,17 @@ def run_server(conn, player_number):
             action_phase(state)
             buy_phase(state, strategy)
             end_turn(state)
+        elif method == "Attack":
+            payload_id = response["id"]
+            payload["id"] = payload_id
+            payload["result"] = {}
+            state.payload = payload
+            handle_attack(state, response["params"])
         elif method == "GameOver":
             print(response["params"])
             break
+        else:
+            print(response)
     conn.close()
 
 
